5b5257336377e9fed7a32cf2a2b4971271d7f8d8
magi <mj7841@srmist.edu.in>
2026-03-04 01:03:31 +0530

    update project

--- helpers/send.ts
+++ helpers/send.ts
@@ -5,9 +5,9 @@ import { animate } from "./animate";
 export default function Send(image: string, options?: Options) {
   return new Response(animate(image, options), {
     headers: {
-      "Accept-Encoding": "gzip, deflate, br, zstd",
-      "cache-control": "private, max-age=43200, stale-if-error=3600, stale-while-revalidate=21600",
+      "cache-control": "public, max-age=0, s-maxage=43200, stale-if-error=3600, stale-while-revalidate=21600",
       "Content-Type": "image/svg+xml",
+      Vary: "Accept-Encoding",
     },
   });
 }
